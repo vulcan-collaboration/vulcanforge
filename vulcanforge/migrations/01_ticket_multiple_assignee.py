81f76707f58d8d9c89253024bc4a1b2985601f7e
William Gaggioli <wgaggioli@gmail.com>
2013-07-10 12:58:24 -0500

    Repaired ticket migration that did not repair ticket history objects

--- vulcanforge/migrations/01_ticket_multiple_assignee.py
+++ vulcanforge/migrations/01_ticket_multiple_assignee.py
@@ -3,19 +3,21 @@ from pylons import app_globals as g, tmpl_context as c
 
 from vulcanforge.common.util.model import pymongo_db_collection
 from vulcanforge.migration.base import BaseMigration
-from vulcanforge.tools.tickets.model import Ticket, Globals, Bin
+from vulcanforge.tools.tickets.model import Ticket, Globals, Bin, TicketHistory
 from vulcanforge.tools.tickets.tasks import add_tickets
 
 
 class TicketMultipleAssignees(BaseMigration):
     def run(self):
-        db, coll = pymongo_db_collection(Ticket)
+        db, coll_ticket = pymongo_db_collection(Ticket)
+        db, coll_ticket_history = pymongo_db_collection(TicketHistory)
         ref_ids = []
         ac_ids = set()
         ticket_count = 0
-        for ticket_doc in coll.find({"assigned_to_id": {"$exists": 1}}):
+        query = {"assigned_to_id": {"$exists": 1}}
+        for ticket_doc in coll_ticket.find(query):
             ticket_doc['assigned_to_ids'] = [ticket_doc.pop('assigned_to_id')]
-            coll.save(ticket_doc)
+            coll_ticket.save(ticket_doc)
             ac_ids.add(ticket_doc['app_config_id'])
             ticket = Ticket.query.get(_id=ticket_doc['_id'])
             ref_ids.append(ticket.index_id())
@@ -24,6 +26,17 @@ class TicketMultipleAssignees(BaseMigration):
         self.write_output('Updated {} tickets'.format(ticket_count))
         ThreadLocalODMSession.flush_all()
         session(Ticket).clear()
+
+        # repare historical dooders
+        hist_count = 0
+        query = {"data.assigned_to_id": {"$exists": 1}}
+        for ticket_hist in coll_ticket_history.find(query):
+            ticket_hist['data']['assigned_to_ids'] = [ticket_hist['data'].pop(
+                'assigned_to_id')]
+            coll_ticket_history.save(ticket_hist)
+            hist_count += 1
+
+        self.write_output('Updated {} ticket histories'.format(hist_count))
 
         bin_count = 0
         global_cur = Globals.query.find({
