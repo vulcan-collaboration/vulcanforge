da81d3cb7702d711b16cb6a1b4bb50b01fab3e8b
J. Tanner Netterville <tannern@gmail.com>
2013-07-10 13:24:09 -0500

    tracker CSV exception handling

--- vulcanforge/tools/tickets/tracker_main.py
+++ vulcanforge/tools/tickets/tracker_main.py
@@ -519,7 +519,9 @@ class TrackerSearchController(BaseController):
             for field in c.app.globals.sortable_custom_fields_shown_in_search()
         ]
         solr_query = kwargs.pop('q', None)
-        solr_sort = kwargs.pop('sort') or 'ticket_num_i asc'
+        solr_sort = kwargs.pop('sort', None)
+        if solr_sort in (None, 'None'):
+            solr_sort = 'ticket_num_i asc'
         solr_result = g.search.search_artifact(TM.Ticket, solr_query,
                                                sort=solr_sort)
 
