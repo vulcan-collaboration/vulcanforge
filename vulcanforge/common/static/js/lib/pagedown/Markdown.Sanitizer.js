34cd8095ff98a672bb1e42f1650626b3991e136e
J. Tanner Netterville <tannern@gmail.com>
2013-07-19 11:04:49 -0500

    javascript markdown sanitizer update

--- vulcanforge/common/static/js/lib/pagedown/Markdown.Sanitizer.js
+++ vulcanforge/common/static/js/lib/pagedown/Markdown.Sanitizer.js
@@ -30,7 +30,7 @@
     var a_white = /^(<a\shref="((https?|ftp):\/\/|\/|#)[-A-Za-z0-9+&@#\/%?=~_|!:,.;\(\)]+"(\stitle="[^"<>]+")?(\sclass="shortlink-placeholder")?\s?>|<\/a>)$/i;
 
     // <img src="url..." optional width  optional height  optional alt  optional title
-    var img_white = /^(<img\ssrc="?(https?:\/\/|\/|blob:http%3A\/\/)[-A-Za-z0-9+&@#\/%?=~_|!:,.;\(\)]+"?(\swidth="\d{1,3}")?(\sheight="\d{1,3}")?(\salt="[^"<>]*")?(\stitle="[^"<>]*")?\s?\/?>)$/i;
+    var img_white = /^(<img\ssrc="?(https?:\/\/|\/|blob:https?%3A\/\/)[-A-Za-z0-9+&@#\/%?=~_|!:,.;\(\)]+"?(\swidth="\d{1,3}")?(\sheight="\d{1,3}")?(\salt="[^"<>]*")?(\stitle="[^"<>]*")?\s?\/?>)$/i;
 
     function sanitizeTag(tag) {
         if (tag.match(basic_tag_whitelist) || tag.match(a_white) || tag.match(img_white))
