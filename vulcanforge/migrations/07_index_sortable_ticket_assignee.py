0933e3f078a00d18fc59946203e62f942ac4137c
William Gaggioli <wgaggioli@gmail.com>
2013-07-26 16:10:41 -0500

    fixed migration for > 10 tix

--- vulcanforge/migrations/07_index_sortable_ticket_assignee.py
+++ vulcanforge/migrations/07_index_sortable_ticket_assignee.py
@@ -4,8 +4,17 @@ from vulcanforge.tools.tickets.tasks import add_tickets
 
 
 class IndexSortableTicketAssignee(BaseMigration):
+    PAGE_SIZE = 500
+
     def run(self):
-        result = g.search('type_s:Ticket AND NOT assigned_to_name_s')
-        ref_ids = [d['id'] for d in result.docs]
-        add_tickets(ref_ids)
-        self.write_output('Reindexed {} tuckets'.format(len(ref_ids)))
+        result = None
+        page = 0
+        while result is None or page * self.PAGE_SIZE < result.hits:
+            result = g.search(
+                'type_s:Ticket AND NOT assigned_to_name_s',
+                rows=self.PAGE_SIZE,
+                page=page)
+            ref_ids = [d['id'] for d in result.docs]
+            add_tickets(ref_ids)
+            page += 1
+        self.write_output('Reindexed {} tuckets'.format(result.hits))
