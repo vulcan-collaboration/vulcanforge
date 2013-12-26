99d675d3e38d6515b797da451fc4c6f418a514f4
William Gaggioli <wgaggioli@gmail.com>
2013-12-26 10:44:02 -0600

    hotfix -- fix old vis migration

--- vulcanforge/migrations/09_sanitize_visualizer_keys.py
+++ vulcanforge/migrations/09_sanitize_visualizer_keys.py
@@ -1,6 +1,9 @@
+import os
+
 from pylons import app_globals as g
 
 from vulcanforge.common.helpers import urlquote
+from vulcanforge.common.util.model import pymongo_db_collection
 from vulcanforge.migration.base import BaseMigration
 from vulcanforge.visualize.model import VisualizerConfig
 
@@ -8,12 +11,15 @@ from vulcanforge.visualize.model import VisualizerConfig
 class SanitizeVisualizerKeys(BaseMigration):
     def run(self):
         count = 0
-        for visualizer in VisualizerConfig.query.find({"widget": "iframe"}):
-            visualizer.widget = 's3'
-            for path in visualizer.bundle_content:
-                if not visualizer.get_s3_key(path, insert_if_missing=False):
-                    key0 = g.get_s3_key(urlquote(visualizer.key_prefix) + path)
-                    key1 = visualizer.get_s3_key(path)
+        db, coll = pymongo_db_collection(VisualizerConfig)
+        for vis_doc in coll.find({"widget": "iframe"}):
+            vis_doc['widget'] = 's3'
+            for path in vis_doc.get('bundle_content', []):
+                prefix = 'Visualizer/' + str(vis_doc['_id'])
+                keyname = prefix + path
+                if not g.get_s3_key(keyname, insert_if_missing=False):
+                    key0 = g.get_s3_key(prefix + '%23' + path)
+                    key1 = g.get_s3_key(keyname)
                     key1.set_contents_from_string(key0.read())
                     key0.delete()
             count += 1
