93d605171e6650c14b2b9f938134f68808b03640
J. Tanner Netterville <tannern@gmail.com>
2014-01-09 12:16:07 -0600

    add purge project command, only works on deleted projects, use wisely

--- vulcanforge/project/model.py
+++ vulcanforge/project/model.py
@@ -1168,13 +1168,12 @@ class AppConfig(MappedClass):
         return None
 
     def load(self):
-        """
-        :returns: the related :class:`Application
-            <vulcanforge.common.app.Application>` instance
-
-        """
         result = g.tool_manager.tools[self.tool_name.lower()]["app"]
         return result
+
+    def instantiate(self):
+        cls = self.load()
+        return cls(self.project, self)
 
     def update_label_counts(self):
         self.label_count_data = self.lookup_label_counts()
